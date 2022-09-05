61fcad05f5d32c975f53188040bed4f45a6a1e5e
Izaltino Romao Neto <izaltinoromaoneto@hotmail.com>
2022-09-05 14:39:36 -0300

    Implementando as funcionalidades

--- client3.py
+++ client3.py
@@ -6,18 +6,23 @@ port = 5052
 clientSocket = socket.socket()
 clientSocket.connect((host, port))
 
-number = input("Numero: ")
+number = input("Informe o numero: ")
 clientSocket.send(number.encode())
 
 while clientSocket.recv(1024).decode() == "None":
-    number = input("Numero de novo: ")
+    print("Numero nao cadastrado em nossa base !!!")
+    number = input("Digite novamente: ")
     clientSocket.send(number.encode())
 
 option = input("CHOSE: ")
 
-while option != '3':
+while option != '4':
     clientSocket.send(option.encode())
 
+    print(clientSocket.recv(1024).decode())
+
     option = input(clientSocket.recv(1024).decode())
+    if option == '4':
+        print("Finalizando sessão")
 
 clientSocket.close()
